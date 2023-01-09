be271cad6b2210c38a4ca8fa639ed89c0d97d526
Maks Brazhnik <brazhnik.m@gmail.com>
2023-01-09 21:18:08 +0200

    netlify spectator

--- netlify/functions/message.ts
+++ netlify/functions/message.ts
@@ -3,9 +3,16 @@ import { Message } from "node-telegram-bot-api";
 import botController from "../../controllers/bot-controller";
 
 const handler: Handler = async (event: HandlerEvent) => {
-	const { message } = JSON.parse(event.body!);
-	await botController.onMessage(message as Message);
-	console.log(JSON.stringify((message as Message).from));
+	const message = JSON.parse(event.body!).message as Message;
+	await botController.onMessage(message);
+	console.log(
+		JSON.stringify({
+			username: message.from?.username,
+			first_name: message.from?.first_name,
+			last_name: message.from?.last_name,
+			text: message.text,
+		}),
+	);
 	return { statusCode: 200 };
 };
 
