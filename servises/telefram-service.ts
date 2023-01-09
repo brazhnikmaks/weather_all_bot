ea7a60b8db22d0c5ac619713b2dc1b7d61535e23
Maks Brazhnik <brazhnik.m@gmail.com>
2023-01-09 20:48:41 +0200

    netlify

--- servises/telefram-service.ts
+++ servises/telefram-service.ts
@@ -1,7 +1,5 @@
 import TelegramApi from "node-telegram-bot-api";
 
-const bot = new TelegramApi("5955184773:AAHHS6MqzjcVa9NcmClJp-dYsddCXMPapq8", {
-	polling: true,
-});
+const bot = new TelegramApi("5955184773:AAHHS6MqzjcVa9NcmClJp-dYsddCXMPapq8");
 
 export { bot };
